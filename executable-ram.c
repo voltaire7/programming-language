49fad5009d54e9ad59f09479084d6956d24b21a8
Ioan Mark Neaga <1932951@cegepmontpetit.ca>
2024-03-25 23:48:36 -0400

    Removed bloat

--- executable-ram.c
+++ executable-ram.c
@@ -3,55 +3,57 @@
 #include <string.h>
 #include <sys/mman.h>
 
-// Define the bytes representing the assembly code to print "Hello, World!"
-// unsigned char code[] = {
-//     0x00, 0x00, 0x80, 0xd2, // mov x0, #0
-//     0x01, 0x00, 0x00, 0x91, // mov x1, #1
-//     0x0d, 0x00, 0x40, 0xd2, // mov x13, #13
-//     0x00, 0x02, 0x00, 0x94, // bl _write
-//     0x00, 0x00, 0x80, 0xd2, // mov x0, #0
-//     0x00, 0x00, 0x00, 0xd4, // svc #0
-// };
-//
-// int (*func)(void) = (int (*)(void))0x12345678;
-// int x = func();
-
-unsigned int code[] = {
-    0x00000000
-};
-
-// Function pointer type for the shellcode
-typedef void (*ShellcodeFunc)();
-
-int main() {
-    // Allocate memory with mmap for the shellcode
-    void *mem = mmap(NULL, sizeof(code), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-    if (mem == MAP_FAILED) {
-        perror("Failed to allocate memory");
-        exit(EXIT_FAILURE);
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <hex_string>\n", argv[0]);
+        return 1;
     }
 
-    // Copy the shellcode into the allocated memory
-    memcpy(mem, code, sizeof(code));
-
-    // Change memory protection to allow execution
-    if (mprotect(mem, sizeof(code), PROT_READ | PROT_EXEC) == -1) {
-        perror("Failed to change memory protection");
-        exit(EXIT_FAILURE);
+    // Get the hexadecimal string from command-line argument
+    const char *hexString = argv[1];
+    size_t hexLength = strlen(hexString);
+    if (hexLength % 2 != 0) {
+        printf("Error: Hexadecimal string length must be even.\n");
+        return 1;
     }
 
-    // Cast the memory address to a function pointer
-    ShellcodeFunc func = (ShellcodeFunc)mem;
-
-    // Call the function
-    func();
-
-    // Cleanup: Unmap the allocated memory
-    if (munmap(mem, sizeof(code)) == -1) {
-        perror("Failed to unmap memory");
-        exit(EXIT_FAILURE);
+    // Convert hexadecimal string to byte array
+    size_t codeSize = hexLength / 2;
+    unsigned char *code = malloc(codeSize);
+    if (code == NULL) {
+        perror("malloc");
+        return 1;
     }
+    for (size_t i = 0; i < hexLength; i += 2) {
+        sscanf(&hexString[i], "%2hhx", &code[i / 2]);
+    }
+
+    int (*addr)() = NULL;
+
+    // Allocate executable buffer
+    addr = mmap(NULL, codeSize, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    // Copy code to buffer
+    memcpy(addr, code, codeSize);
+
+    // Modify the protection to allow execution
+    if (mprotect(addr, sizeof(code), PROT_EXEC) == -1) {
+        perror("mprotect");
+        return 1;
+    }
+
+    // Run code
+    addr();
+
+    // Free allocated memory
+    free(code);
+
+    // Unmap memory
+    munmap(addr, codeSize);
 
     return 0;
 }
-
